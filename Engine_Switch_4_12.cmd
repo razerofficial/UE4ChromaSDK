9cfbd98119a95f6c1b53bbee812b2269aa3342f1
Tim Graupmann <tgraupmann@gmail.com>
2017-10-13 18:53:23 -0700

    Open zip to the right folder

--- Engine_Switch_4_12.cmd
+++ Engine_Switch_4_12.cmd
@@ -6,4 +6,4 @@ IF NOT EXIST %APP% ECHO UnrealEngineVersionChanger needs to be compiled!
 IF EXIST %APP% CALL "UnrealEngineVersionChanger\bin\Debug\UnrealEngineVersionChanger.exe" 4.12
 COPY /Y UE4ChromaSDK_4_12.uproject UE4ChromaSDK.uproject
 CALL SwitchEngineVersion.cmd
-START "" ChromaSDKPlugin_4_12.zip
+"C:\Program Files\7-Zip\7zFM.exe" "%CD%\ChromaSDKPlugin_4_12.zip\ChromaSDKPlugin\Binaries\Win64\"
--- Engine_Switch_4_14.cmd
+++ Engine_Switch_4_14.cmd
@@ -5,4 +5,4 @@ IF NOT EXIST %APP% ECHO UnrealEngineVersionChanger needs to be compiled!
 IF EXIST %APP% CALL "UnrealEngineVersionChanger\bin\Debug\UnrealEngineVersionChanger.exe" 4.15
 COPY /Y UE4ChromaSDK_4_14.uproject UE4ChromaSDK.uproject
 CALL SwitchEngineVersion.cmd
-START "" ChromaSDKPlugin_4_14.zip
+"C:\Program Files\7-Zip\7zFM.exe" "%CD%\ChromaSDKPlugin_4_14.zip\ChromaSDKPlugin\Binaries\Win64\"
